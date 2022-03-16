6b79fdf2413c82530bff355f97d50bc82fa4693a
Lukasz Janyst <lukasz@jany.st>
2022-03-16 09:28:12 +0100

    Improve the Polyline3d example

--- examples/06-languagelessness/polyline3d/polyline3d.go
+++ examples/06-languagelessness/polyline3d/polyline3d.go
@@ -5,9 +5,27 @@ import (
 	"github.com/ljanyst/ghostscad/sys"
 
 	. "github.com/go-gl/mathgl/mgl64"
+	. "github.com/ljanyst/ghostscad/lib/degmath"
+	. "github.com/ljanyst/ghostscad/primitive"
 )
 
 func main() {
-	points := []Vec3{{1, 2, 3}, {4, -5, -6}, {-1, -3, -5}, {0, 0, 0}}
-	sys.RenderOne(shapes.NewPolyline3d(points, 1).Build())
+	r := 50.0
+	points := []Vec3{}
+	for a := 0.0; a <= 180; a++ {
+		points = append(points, Vec3{
+			r * Cos(-90.0+a) * Cos(a),
+			r * Cos(-90.0+a) * Sin(a),
+			r * Sin(-90.0+a),
+		})
+	}
+
+	list := NewList()
+
+	for i := 0; i < 8; i++ {
+		list.Add(
+			NewRotation(Vec3{0, 0, float64(i * 45)}, shapes.NewPolyline3d(points, 2).Build()),
+		)
+	}
+	sys.RenderOne(list)
 }
